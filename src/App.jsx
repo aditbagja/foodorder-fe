d9691214e7f85bcaaca3394f8126cc130b727665
aditbagja <aditbagja44@gmail.com>
2024-04-19 11:25:09 +0700

    fix jsx parent tag

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,20 @@ import OrderDetail from "./pages/OrderDetail";
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/resto/:id" element={<Resto />} />
-            <Route path="/keranjang" element={<Keranjang />} />
-            <Route path="/order-progress" element={<Order />} />
-            <Route path="/order-list" element={<OrderList />} />
-            <Route path="/order/:id" element={<OrderDetail />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/resto/:id" element={<Resto />} />
+          <Route path="/keranjang" element={<Keranjang />} />
+          <Route path="/order-progress" element={<Order />} />
+          <Route path="/order-list" element={<OrderList />} />
+          <Route path="/order/:id" element={<OrderDetail />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
